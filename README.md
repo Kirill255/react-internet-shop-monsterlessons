3f2cc8b509232002fd54fcf9f876b732deacb7ef
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-04 21:58:52 +0300

    add withLayout hoc

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 # react-internet-shop-monsterlessons
+
+https://coursehunters.net/course/internet-magazin-na-react-redux
+
+## Other
+
+https://stackoverflow.com/questions/42862028/react-router-v4-with-multiple-layouts
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import Phones from "./components/Phones";
+
+import withLayout from "./hoc/withLayout";
+
 const App = () => {
   return (
     <div>
       <Switch>
-        <Route path="/" exact render={() => <h1>Hello world</h1>} />
+        <Route path="/" exact render={withLayout(Phones)} />
         <Route path="*" render={() => <h1>Not Found Page</h1>} />
       </Switch>
     </div>
